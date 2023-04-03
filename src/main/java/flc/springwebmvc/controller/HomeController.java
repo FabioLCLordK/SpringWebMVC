95e0cd6b689ed5217fc3bd58ecf2dceda3b7143e
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-04-03 10:48:42 +0200

    Rev 1.3

--- src/main/java/flc/springwebmvc/controller/HomeController.java
+++ src/main/java/flc/springwebmvc/controller/HomeController.java
@@ -9,18 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @Autowired
     private HomeService homeService;
-    
-   
+     
     
 //    @Override
 //    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response){
@@ -31,11 +32,21 @@ public class HomeController {
     
     //OPPURE
     
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView readPage(){
         
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("message", homeService.sayHello());
+        return modelAndView;
+    }
+    
+    //@RequestMapping(value = "/about", method = RequestMethod.GET)
+    //oppure invece
+    @GetMapping(value ="/about")
+    public ModelAndView about(){
+        
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("message", "roba riguardo noi");
         return modelAndView;
     }
     
